103002db409b2a606629690d0177276def733bdd
Jonas Hagedorn <hagedorn.j@gmx.de>
2021-05-31 19:48:24 +0200

    add routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [ :show, :index ]
-  get 'users/:id/friendships', to: 'friendships#create'
+  post 'users/:id/friendships', to: 'friendships#create'
 
   resources :challenges, only: [ :new, :create, :index, :show ] do
     resources :challenge_users, only: [ :new, :create ]
