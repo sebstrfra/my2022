722ccebdef5e2fc74f9dcf7a36ffa5698e992926
Jonas Hagedorn <hagedorn.j@gmx.de>
2021-05-31 16:02:40 +0200

    add user models - edit 1

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,7 +1,7 @@
 class Challenge < ApplicationRecord
   belongs_to :user
-  has_many :users, through: :challenge_users
   has_many :challenge_users
+  has_many :users, through: :challenge_users
   has_many :goals
   has_many :user_goals, through: :goals
 end
