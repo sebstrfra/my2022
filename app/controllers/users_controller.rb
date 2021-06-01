d59aeeb21f5de6de3b20aaffe40cf525720d8b61
Jonas Hagedorn <hagedorn.j@gmx.de>
2021-06-01 14:17:36 +0200

    delete one index function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
+  
   def index
     if params[:query].present?
       @users = User.where("first_name ILIKE ?", "%#{params[:query]}%")
