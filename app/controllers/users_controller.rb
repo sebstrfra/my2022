d74fd06b061e606bfa77f572f2ea00f70ee14cab
Jonas Hagedorn <hagedorn.j@gmx.de>
2021-06-01 14:18:44 +0200

    show no users when we do not search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  
+
   def index
     if params[:query].present?
       @users = User.where("first_name ILIKE ?", "%#{params[:query]}%")
     else
-      @users = User.all
+      @users = []
     end
   end
 
